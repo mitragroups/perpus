01608dd6f40e972b7d08954e982aa5177262b1e2
mitragroups <miftahmuhajir@gmail.com>
2023-11-12 20:50:02 +0700

    Branch Develop

--- db/migrate/20231112134144_create_books.rb
+++ db/migrate/20231112134144_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
       t.string :title, default: 'Belum ada judul buku'
-      t.integer :price, limit: 100
+      t.integer :price
       t.integer :page, null: false
       t.text :description
 
