22c4b4b1e80b8b28692e9ce60a65c9df3e8ebf3c
mitragroups <miftahmuhajir@gmail.com>
2023-12-27 10:17:46 +0700

    Seeding Authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+=begin
 Book.create(title: 'Go-Lang', price: 60_000, page: 60, description: 'Bahasa pemrograman dari Google')
 Book.create(title: 'Java Springboot', price: 475_000, page: 475, description: 'Framework Springboot untuk Java')
 Book.create(title: 'Laravel', price: 250_000, page: 250, description: 'PHP Framework for Web Development')
 Book.create(title: 'Django', price: 300_000, page: 300, description: 'Python Framework for Web')
 Book.create(title: 'Flutter', page: 210, price: 210_000, description: 'Mobile Hybrid Development')
-
-=begin
-Author.create(name: 'Mafaaza', age: 5, address: 'Tebing Tinggi', gender: '')
-Author.create(name: "Mu'ammar", age: 8, address: 'Jakarta', gender: '')
-Author.create(name: 'Mahreen', age: 3, address: 'Bogor', gender: 'Female')
 =end
+
+#=begin
+Author.create(name: 'Miftah MD', age: 35, address: Jakarta, gender: 'Male')
+Author.create(name: 'Mafaaza', age: 5, address: 'Tebing Tinggi', gender: 'Female')
+Author.create(name: "Mu'ammar", age: 8, address: 'Jakarta', gender: 'Male')
+Author.create(name: 'Mahreen', age: 3, address: 'Bogor', gender: 'Female')
+#=end
