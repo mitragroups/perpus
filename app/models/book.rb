ad4a6dc61681e9ae951d6bce08b0cd185e11688c
mitragroups <miftahmuhajir@gmail.com>
2023-12-28 15:23:33 +0700

    Add Method buku_mahal & urutan_buku_termahal in Model Book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,13 @@
 class Book < ApplicationRecord
     belongs_to :author
+
+    def self.urutan_buku_termahal
+        order(price: :desc).pluck(:title, :price, :page)
+    end
+
+    def self.buku_mahal
+        where('price > 300000')
+    end
+    
+    
 end
