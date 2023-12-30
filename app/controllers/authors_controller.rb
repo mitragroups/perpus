2d1b8ceff47146d774032ef0c1e4d1e26098bb5e
mitragroups <miftahmuhajir@gmail.com>
2023-12-30 13:57:37 +0700

    Add method oldest in Authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,12 @@
 class AuthorsController < ApplicationController
   def index
   end
+
+  def tertua 
+    @tertua = Author.oldest
+    render json: {
+      values: @tertua, 
+      message: 'Success'
+    }, status: 200
+  end
 end
