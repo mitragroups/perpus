7b1c14eee9e438e988da2f293fbddcf3b94534c1
mitragroups <miftahmuhajir@gmail.com>
2023-12-30 13:40:52 +0700

    Add Method author_male & author_female

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,11 @@
 class Author < ApplicationRecord
     has_many :books
+
+    def self.author_male
+        where("gender != 'Male'")
+    end
+    
+    def self.author_female 
+        where("gender = 'Female'").pluck(:name)
+    end
 end
