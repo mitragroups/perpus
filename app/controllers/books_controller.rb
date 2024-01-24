f45676b7c6f422528f501a505b184fe1f0c9c772
mitragroups <miftahmuhajir@gmail.com>
2024-01-25 02:03:10 +0700

    Add books#show json

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,14 @@ class BooksController < ApplicationController
   def index
     @books = Book.all 
   end
+
+  def show 
+    id = params[:id]
+    @book = Book.find(id)
+    render json: {
+      values: @book,
+      message: 'Success'
+    }, status: 200
+  end
+  
 end
